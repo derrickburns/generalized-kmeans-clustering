3d4da43404f36fe278cf583a27c5096326c67e32
Derrick Burns <derrickrburns@gmail.com>
2015-02-11 22:14:00 -0800

    Use add only centroid maker,

--- src/main/scala/com/massivedatascience/clusterer/ColumnTrackingKMeans.scala
+++ src/main/scala/com/massivedatascience/clusterer/ColumnTrackingKMeans.scala
@@ -81,7 +81,7 @@ class ColumnTrackingKMeans(
 
   import ColumnTrackingKMeans._
 
-  val addOnly = false
+  val addOnly = true
 
   /**
    * count number of points assigned to each cluster
@@ -261,7 +261,7 @@ class ColumnTrackingKMeans(
      * @param previousAssignments previous assignments
 
      * @return changes to cluster position
-     */
+
 
 
     def getExactCentroidChanges(
@@ -286,68 +286,69 @@ class ColumnTrackingKMeans(
         if (curr.cluster != prev.cluster) curr.cluster else -1
       }
     }
+     */
 
-    /*
-
-def getStochasticCentroidChanges(
-  points: RDD[BregmanPoint],
-  assignments: RDD[Assignment]): Map[Int, MutableWeightedVector] =
-
-  points.zip(assignments).filter(_._2.isAssigned).map { case (o, p) =>
-    (p.cluster, o)
-  }.aggregateByKey(pointOps.getCentroid)(_.add(_), _.add(_)).collectAsMap()
-
-
-def getExactCentroidChanges(
-  points: RDD[BregmanPoint],
-  currentAssignments: RDD[Assignment],
-  previousAssignments: RDD[Assignment]): Array[(Int, MutableWeightedVector)] = {
-
-  require(points.getStorageLevel.useMemory)
-  require(currentAssignments.getStorageLevel.useMemory)
-  require(previousAssignments.getStorageLevel.useMemory)
-
-  val changes = currentAssignments.zip(previousAssignments).map { case (curr, prev) =>
-    (curr.cluster, prev.cluster)
-  }.setName("changes").cache()
-
-  val results = points.zip(changes).mapPartitions { pts =>
-    val buffer = new ArrayBuffer[(Int, CentroidChange)]
-    for ((point, (add, sub)) <- pts) {
-      if (add != sub) {
-        if (add >= 0) buffer.append((add, Add(point)))
-        if (sub >= 0) buffer.append((sub, Sub(point)))
-      }
-    }
-    logInfo(s"buffer size ${buffer.size}")
-    buffer.iterator
-  }.aggregateByKey(pointOps.getCentroid)(
-      (x, y) => y match {
-        case Add(p) => x.add(p)
-        case Sub(p) => x.sub(p)
-      },
-      (x, y) => x.add(y)
-    ).collect()
-
-  changes.unpersist()
-  results
-}
-
-*/
 
     def getStochasticCentroidChanges(
       points: RDD[BregmanPoint],
-      assignments: RDD[Assignment]): Array[(Int, MutableWeightedVector)] = {
-
-      require(points.getStorageLevel.useMemory)
-      require(assignments.getStorageLevel.useMemory)
+      assignments: RDD[Assignment]): Map[Int, MutableWeightedVector] =
 
       points.zip(assignments).filter(_._2.isAssigned).map { case (o, p) =>
         (p.cluster, o)
-      }.aggregateByKey(pointOps.getCentroid)(_.add(_), _.add(_)).collect()
+      }.aggregateByKey(pointOps.getCentroid)(_.add(_), _.add(_)).collectAsMap()
 
+
+    def getExactCentroidChanges(
+      points: RDD[BregmanPoint],
+      currentAssignments: RDD[Assignment],
+      previousAssignments: RDD[Assignment]): Array[(Int, MutableWeightedVector)] = {
+
+      require(points.getStorageLevel.useMemory)
+      require(currentAssignments.getStorageLevel.useMemory)
+      require(previousAssignments.getStorageLevel.useMemory)
+
+      val changes = currentAssignments.zip(previousAssignments).map { case (curr, prev) =>
+        (curr.cluster, prev.cluster)
+      }.setName("changes").cache()
+
+      val results = points.zip(changes).mapPartitions { pts =>
+        val buffer = new ArrayBuffer[(Int, CentroidChange)]
+        for ((point, (add, sub)) <- pts) {
+          if (add != sub) {
+            if (add >= 0) buffer.append((add, Add(point)))
+            if (sub >= 0) buffer.append((sub, Sub(point)))
+          }
+        }
+        logInfo(s"buffer size ${buffer.size}")
+        buffer.iterator
+      }.aggregateByKey(pointOps.getCentroid)(
+          (x, y) => y match {
+            case Add(p) => x.add(p)
+            case Sub(p) => x.sub(p)
+          },
+          (x, y) => x.add(y)
+        ).collect()
+
+      changes.unpersist()
+      results
     }
 
+    /*
+
+      def getStochasticCentroidChanges(
+        points: RDD[BregmanPoint],
+        assignments: RDD[Assignment]): Array[(Int, MutableWeightedVector)] = {
+
+        require(points.getStorageLevel.useMemory)
+        require(assignments.getStorageLevel.useMemory)
+
+        points.zip(assignments).filter(_._2.isAssigned).map { case (o, p) =>
+          (p.cluster, o)
+        }.aggregateByKey(pointOps.getCentroid)(_.add(_), _.add(_)).collect()
+
+      }
+
+  */
 
     /**
      * Find the closest cluster from a given set of clusters
@@ -495,7 +496,7 @@ def getExactCentroidChanges(
       previousAssignments: RDD[Assignment]): RDD[Assignment] = {
 
       require(assignments.getStorageLevel.useMemory)
-      
+
       val newCenters = updateCenters(round, assignments, previousAssignments, centers)
       previousAssignments.unpersist()
       val newAssignments = updatedAssignments(round, assignments, newCenters)
