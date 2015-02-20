450410a13c431f9530cf05d2596b56f104fd980f
Derrick Burns <derrickrburns@gmail.com>
2015-02-20 00:06:30 -0800

    Revert smoothing change.  Just smooth centers.

--- src/main/scala/com/massivedatascience/clusterer/BasicPointOps.scala
+++ src/main/scala/com/massivedatascience/clusterer/BasicPointOps.scala
@@ -180,17 +180,16 @@ object DiscreteDenseKLPointOps extends BasicPointOps(NaturalKLDivergence)
  */
 object DiscreteDenseSmoothedKLPointOps extends BasicPointOps(NaturalKLDivergence) {
 
-  override def vectorToPoint(v: Vector): BregmanPoint = {
-    val h = add(v, 1.0)
-    val weight = v.toArray.sum + v.size
-    new BregmanPoint(h, weight, divergence.F(h, weight))
+  override def vectorToPoint(h: Vector): BregmanPoint = {
+    val w = h.toArray.sum
+    new BregmanPoint(h, w, divergence.F(h, w))
   }
 
   override def toCenter(v: WeightedVector): BregmanCenter = {
-    val h = v.homogeneous
-    val w = v.weight
+    val h = add(v.homogeneous, 1.0)
+    val w = v.weight + v.homogeneous.size
     val df = divergence.gradF(h, w)
-    new BregmanCenter(v.homogeneous, v.weight, dot(h, df) / w - divergence.F(h, w), df)
+    new BregmanCenter(h, w, dot(h, df) / w - divergence.F(h, w), df)
   }
 }
 
