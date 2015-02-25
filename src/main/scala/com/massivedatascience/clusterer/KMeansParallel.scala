1f462f95dcb42b73ddf38420ad3e537689a67a7f
Derrick Burns <derrickrburns@gmail.com>
2015-02-24 19:00:05 -0800

    Modified comments.

--- src/main/scala/com/massivedatascience/clusterer/KMeansParallel.scala
+++ src/main/scala/com/massivedatascience/clusterer/KMeansParallel.scala
@@ -14,7 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * This code is a complete re-write version of the original Spark 1.0.2 implementation.
+ * This code is a complete re-write version of the original Spark 1.0.2 implementation by
+ * Derrick R. Burns.
  *
  *
  */
